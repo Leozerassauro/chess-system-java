5fd937379dc9fe073374d94ab15a8dbf55dc2da8
Leonardo Girardi <leo.girardi97@hotmail.com>
2020-03-19 17:31:41 -0300

    BoardException and defensive programming

---
+++ src/boardgame/BoardException.java
@@ -0,0 +1,13 @@
+package boardgame;
+
+/*
+ * @author Leonardo
+ */
+public class BoardException extends RuntimeException {
+
+    //constructor
+    public BoardException(String msg) {
+        super(msg);
+    }
+
+}
