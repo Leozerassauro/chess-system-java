e9bc9d1d4a88a959f004695d3f16203e26385779
Leonardo Girardi <leo.girardi97@hotmail.com>
2020-03-19 16:06:12 -0300

    First class: Position

---
+++ src/boardgame/Position.java
@@ -0,0 +1,42 @@
+package boardgame;
+
+/*
+ * @author Leonardo
+ */
+public class Position {
+
+    private int row;
+    private int column;
+
+    //constructor
+    public Position(int row, int column) {
+        this.row = row;
+        this.column = column;
+    }
+
+    //getters and setters
+    public int getRow() {
+        return row;
+    }
+
+    public void setRow(int row) {
+        this.row = row;
+    }
+
+    public int getColumn() {
+        return column;
+    }
+
+    public void setColumn(int column) {
+        this.column = column;
+    }
+    
+    //toString
+
+    @Override
+    public String toString() {
+        return row + ", " + column;
+    }
+    
+
+}
