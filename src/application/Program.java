bad484b0f7dc0ab375526279b978654b570da449
Leonardo Girardi <leo.girardi97@hotmail.com>
2020-03-19 16:00:55 -0300

    Project created

---
+++ src/application/Program.java
@@ -0,0 +1,13 @@
+package application;
+
+/*
+ * @author Leonardo
+ */
+public class Program {
+
+    public static void main(String[] args) {
+        
+        System.out.println("Hello World!");
+    }
+
+}
