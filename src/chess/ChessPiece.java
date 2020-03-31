3d83c4409c16bad09b5f2a2581d31cb2dd136de6
Leonardo Girardi <leo.girardi97@hotmail.com>
2020-03-31 15:17:27 -0300

    Piece move count

--- src/chess/ChessPiece.java
+++ src/chess/ChessPiece.java
@@ -11,6 +11,7 @@ public abstract class ChessPiece extends Piece {
 
     //attributes
     private Color color;
+    private int moveCount;
 
     //constructor
     public ChessPiece(Color color, Board board) {
@@ -22,12 +23,24 @@ public abstract class ChessPiece extends Piece {
     public Color getColor() {
         return color;
     }
-    
+
+    public int getMoveCount() {
+        return moveCount;
+    }
+
     public ChessPosition getChessPosition() {
         return ChessPosition.fromPosition(position);
     }
 
     //methods
+    public void increaseMoveCount() {
+        moveCount++;
+    }
+
+    public void decreaseMoveCount() {
+        moveCount--;
+    }
+
     protected boolean isThereOpponentPiece(Position position) {
         ChessPiece p = (ChessPiece) getBoard().piece(position);
         return p != null && p.getColor() != color;
