1bbd73d612af234f4ccc410647dcc2c708528972
Leonardo Girardi <leo.girardi97@hotmail.com>
2020-04-01 15:51:46 -0300

    Special move - Promotion

--- src/application/Program.java
+++ src/application/Program.java
@@ -13,13 +13,13 @@ import java.util.Scanner;
  * @author Leonardo
  */
 public class Program {
-    
+
     public static void main(String[] args) {
-        
+
         Scanner sc = new Scanner(System.in);
         ChessMatch chessMatch = new ChessMatch();
         List<ChessPiece> captured = new ArrayList<>();
-        
+
         while (!chessMatch.getCheckMate()) {
             try {
                 UI.clearScreen();;
@@ -27,19 +27,25 @@ public class Program {
                 System.out.println("");
                 System.out.print("Source: ");
                 ChessPosition source = UI.readChessPosition(sc);
-                
+
                 boolean[][] possibleMoves = chessMatch.possibleMoves(source);
                 UI.clearScreen();
                 UI.printBoard(chessMatch.getPieces(), possibleMoves);
-                
+
                 System.out.println("");
                 System.out.print("Target: ");
                 ChessPosition target = UI.readChessPosition(sc);
-                
+
                 ChessPiece capturedPiece = chessMatch.performChessMove(source, target);
-                
+
                 if (capturedPiece != null) {
                     captured.add(capturedPiece);
+                }
+
+                if (chessMatch.getPromoted() != null) {
+                    System.out.println("Enter piece for promotion (B/N/R/Q): ");
+                    String type = sc.nextLine();
+                    chessMatch.replacePromotedPiece(type);
                 }
             } catch (ChessException e) {
                 System.out.println(e.getMessage());
@@ -50,6 +56,6 @@ public class Program {
             }
         }
         UI.clearScreen();
-        UI.printMatch(chessMatch, captured);        
+        UI.printMatch(chessMatch, captured);
     }
 }
